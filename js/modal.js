ee1cec21975b04426fdae08f23b3d7bec37e9470
ggso1 <24g_shsr@liceum.ztu.edu.ua>
2025-02-26 09:56:52 +0200

    css

--- js/modal.js
+++ js/modal.js
@@ -14,4 +14,13 @@ closeModalBtn.addEventListener('click', () => {
     modalBackdrop.classList.add('is-hidden');
 });
 
+document.querySelector('.form-button').addEventListener('click', function (event) {
+    // Запобігаємо стандартному відправленню форми
+    event.preventDefault();
+
+    // Перехід на інший сайт
+    window.location.href = "https://ru.freepik.com/free-photos-vectors/%D1%84%D0%BE%D0%BD-%D0%B1%D1%83%D0%B4%D1%83%D1%89%D0%B5%D0%B5";  // Замініть на ваш URL
+});
+
+
 
